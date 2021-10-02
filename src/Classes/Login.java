e23f7ecab5d494c2618615fce9d6d477475d25b9
AaroT47 <38485570+AaroT47@users.noreply.github.com>
2021-10-02 18:40:41 +0545

    "Login Panel Added"

---
+++ src/Classes/Login.java
@@ -0,0 +1,47 @@
+package Classes;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+
+public class Login extends JFrame {
+
+    private JPanel contentPane;
+    private JTextField ShopPassword;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Login frame = new Login();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+}
