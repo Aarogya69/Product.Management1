661d63fc253e922f90ed524914705fe58605ef21
AaroT47 <38485570+AaroT47@users.noreply.github.com>
2021-10-02 19:13:49 +0545

    "Frame Added"

--- src/Classes/Login.java
+++ src/Classes/Login.java
@@ -36,12 +36,43 @@ public class Login extends JFrame {
         });
     }
 
+    /**
+     * Create the frame.
+     */
+    public Login() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 800, 486);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("Login");
+        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        lblNewLabel.setBounds(330, 99, 79, 50);
+        contentPane.add(lblNewLabel);
+
+        JLabel lblNewLabel_1 = new JLabel("User");
+        lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblNewLabel_1.setBounds(262, 184, 46, 34);
+        contentPane.add(lblNewLabel_1);
+
+        JComboBox ShopUser = new JComboBox();
+
+        ShopUser.setModel(new DefaultComboBoxModel(new String[]{"User"}));
+        ShopUser.setBounds(340, 193, 112, 22);
+        contentPane.add(ShopUser);
+
+        JLabel ShopPasswordLabel = new JLabel("Password");
+        ShopPasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        ShopPasswordLabel.setBounds(242, 255, 88, 34);
+        contentPane.add(ShopPasswordLabel);
+
+        ShopPassword = new JTextField();
+        ShopPassword.setBounds(340, 265, 112, 20);
+        contentPane.add(ShopPassword);
+        ShopPassword.setColumns(10);
 
 
-
-
-
-
-
-
+    }
 }
